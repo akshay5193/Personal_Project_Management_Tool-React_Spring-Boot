8e302360e175f187cd463bbafe6914d6361faa31
Akshay Nande <akshaytnande@gmail.com>
2019-09-03 18:45:50 -0700

    initial commit

---
+++ PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/PersonalProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ PersonalProjectManagementTool/src/test/java/com/akshay/ppmtool/PersonalProjectManagementToolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.akshay.ppmtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalProjectManagementToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
