8cc30b68072c36444ec25dab5e61f61a0f0c5e7f
Akshay Nande <akshaytnande@gmail.com>
2019-09-13 05:49:04 -0700

    Create React front end project and add components for dashboard, add project and navbar

---
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+    state = {}
+    render() {
+        return (
+            <h1>Add Project Form</h1>
+        );
+    }
+}
+
+export default AddProject;
