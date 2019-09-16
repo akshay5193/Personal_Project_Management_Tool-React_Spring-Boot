c766e0ef4325043073321f8f8e6c300fc832de5e
Akshay Nande <akshaytnande@gmail.com>
2019-09-15 19:18:23 -0700

    display all projects  on dashboard working

--- ppmtool-react-client/src/components/dashboard.js
+++ ppmtool-react-client/src/components/dashboard.js
@@ -49,6 +49,15 @@ class Dashboard extends Component {
     }
 
     render() {
+
+        // const projectObject = {
+        //     projectName: "Project Name PROPS",
+        //     projectIdentifier: "PROP",
+        //     description: "description from PROPS"
+        // };
+
+        const { projects } = this.props.project;
+
         return (
             <div className="projects">
                 <div className="container">
@@ -60,7 +69,13 @@ class Dashboard extends Component {
 
                             <br />
                             <hr />
-                            <ProjectItem />
+                            {
+                                projects.map(project => (
+                                    <ProjectItem key={project.id} project={project} ></ProjectItem>
+                                ))
+
+                            }
+
                         </div>
                     </div>
                 </div>
