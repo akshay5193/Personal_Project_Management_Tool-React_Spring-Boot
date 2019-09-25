036b9314286961fc5412020e01a32aa0229447cf
Akshay Nande <akshaytnande@gmail.com>
2019-09-24 22:26:37 -0700

    create relationship between the project backlog and the project tasks

--- PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/domains/Backlog.java
+++ PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/domains/Backlog.java
@@ -1,11 +1,16 @@
 package com.akshay.ppmtool.domains;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -27,7 +32,8 @@ public class Backlog {
 	
 	
 //	One-To-Many with project tasks
-	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+	private List<ProjectTask> projectTasks = new ArrayList<>();
 	
 
 	public Backlog() {
--- PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/domains/ProjectTask.java
+++ PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/domains/ProjectTask.java
@@ -2,14 +2,20 @@ package com.akshay.ppmtool.domains;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ProjectTask {
@@ -27,6 +33,12 @@ public class ProjectTask {
 	private Date dueDate;
 	
 //	Many-To-One with the backlog
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	@JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+	@JsonIgnore
+	private Backlog backlog;
+	
+	
 	@Column(updatable = false)
 	private String projectIdentifier;
 	
@@ -157,6 +169,21 @@ public class ProjectTask {
 
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
+	}
+	
+	
+	
+
+
+
+	public Backlog getBacklog() {
+		return backlog;
+	}
+
+
+
+	public void setBacklog(Backlog backlog) {
+		this.backlog = backlog;
 	}
 
 
