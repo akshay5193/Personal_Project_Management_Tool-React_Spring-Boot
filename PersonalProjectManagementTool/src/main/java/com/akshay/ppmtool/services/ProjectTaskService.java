e28e05892684f3ca9ff0158e005fbc764c64ad33
Akshay Nande <akshaytnande@gmail.com>
2019-10-22 20:46:00 -0700

    validation for wrond project id and backlog id

--- PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/services/ProjectTaskService.java
+++ PersonalProjectManagementTool/src/main/java/com/akshay/ppmtool/services/ProjectTaskService.java
@@ -26,9 +26,6 @@ public class ProjectTaskService {
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 		
 		try {
-			// Project Not Found Exception
-			
-			
 			// PTs to be added to a specific project, project != null, BL exists
 			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 			
@@ -77,9 +74,26 @@ public class ProjectTaskService {
 	
 	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 		
-		// make sure we are searching on the right backlog
+		// make sure we are searching on the right backlog which exists
+		Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
 		
-		return projectTaskRepository.findByProjectSequence(pt_id);
+		if (backlog == null) {
+			throw new ProjectNotFoundException("project with ID: " + backlog_id + " does not exist.");
+		}
+		
+		// make sure that our task exist
+		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+		
+		if (projectTask == null) {
+			throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+		}
+		
+		// make sure that the backlog/project id in the path corresponds to the right project
+		if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
+			throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in the project: '" + backlog_id + "'");
+		}
+		
+		return projectTask;
 	}
 
 }
